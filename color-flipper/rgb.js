1c77b89ea75b76e928c58b2bb96423316c8743a1
PRABHLEEN-KAUR3 <prabhleen-kaur3@myseneca.ca>
2023-01-11 16:38:10 -0500

    Added color-flipper

--- color-flipper/rgb.js
+++ color-flipper/rgb.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-    let rgbColor = "rgb("
+    let rgbColor = "rgb(";
     let x = getRandNumber(0, 256);
     let y = getRandNumber(0, 256);
     const z = getRandNumber(0, 256);
